cbd0d8c72ba56adb0c867552be4b608e9349c6b7
Danielsodera <daniel_sodera@me.com>
2025-01-21 09:10:56 -0500

    Added dummy track data to SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,6 +2,13 @@ import React, {useState} from "react";
 import SearchBar from "./SearchBar";
 import TrackList from "./TrackList";
 
+const trackData = [
+    {id: "1", artist: "kendrick lamar", album: "good kid maad city", song: "poetic justice"},
+    {id: "2", artist: "kendrick lamar", album: "good kid maad city", song: "the art of peer pressure"},
+    {id: "3", artist: "kendrick lamar", album: "good kid maad city", song: "money trees"},
+    {id: "4", artist: "kendrick lamar", album: "good kid maad city", song: "sing about me, i'm dying of thirst"}
+]
+
 export default function SearchResults(){
 
     const [searchResults, setSearchResults] = useState([]);
