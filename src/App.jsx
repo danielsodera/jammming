216a3de02451b9f1d25dbf32230e65c3b8ceb4a6
Danielsodera <daniel_sodera@me.com>
2025-01-21 08:02:24 -0500

    Added title, text and search bar with functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
   
 
   return (
     <>
-    
-  
-   </>
+    <header>
+    <h1>Jammming!</h1>
+    <p>Create your own Spotify Playlist</p><br /> 
+    </header>
+    <SearchBar />
+    </>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchBar(){
 
+    //set useState variable and user input 
+    const [search, setSearch] = useState("");
+    let userInput = ""
+
+    //Capture what was entered in search bar and store it in userInput
+    function handleSearchChange(e){
+        userInput = e.target.value
+    }
+
+    //Only store userInput in search variable when the user clicks the search button
+    function handleClick(){
+        setSearch(userInput)
+        console.log(userInput)
+    }
 
 
     return (
-        <>
-        <button>Search</button>
-        </>
+        <div className="search-container">
+        <input id="searchBar" type="text" placeholder="Search for a song..." onChange={(e) => handleSearchChange(e)}/><br /> 
+        <button onClick={handleClick}>Search</button>
+        </div>
     )
 }
